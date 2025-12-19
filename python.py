849f955e0ce69f22c035716f70d3afe1f474b235
kalisonUmich <kalison@umich.edu>
2025-12-18 23:02:14 -0500

    Add files via upload
    Added the python example file

---
+++ python.py
@@ -0,0 +1,27 @@
+# This file goes over some fundamental basic programming concepts.
+
+
+# Variables let you store information. Unlike other languages you don't need to specify the type (string, int, double)
+
+#these are both type str (for string). they must be surrounded by quotes
+word = "word"
+sentence = "this is a sentence!"
+
+#this is an int. (integer) it has to be a whole number and can be positive or negative.
+num = 3
+
+#this is a double. it can have decimal points and alsp be positive or negative
+another_num = 5.5
+
+
+
+# The 'print' function prints output to the screen with a newline on the end
+print("Hello world")
+
+
+#you can to operations on numbers and assign the solution to a variable or print it.
+print("the solution of 5 + 3 is")
+print(5 + 3)
+
+subtraction = 7 - 2
+print("this number is " + str(subtraction))
